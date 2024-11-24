a191b86c66386d18eef0208f760bd42e6cba1981
huoshan <1726941245@qq.com>
2024-11-24 23:57:06 +0800

    feat: 全局eslint配置

---
+++ .eslintrc.ts
@@ -0,0 +1,24 @@
+const config = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended'
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/explicit-function-return-type': 'warn',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/explicit-module-boundary-types': 'warn'
+  }
+};
+
+export default config;
