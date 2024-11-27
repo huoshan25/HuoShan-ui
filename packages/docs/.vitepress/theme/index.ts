82be8dd1cdfa3b436217ab16edce42ab078bf7ad
huoshan <1726941245@qq.com>
2024-11-27 12:07:02 +0800

    feat: docs移出ui组件使用

--- packages/docs/.vitepress/theme/index.ts
+++ packages/docs/.vitepress/theme/index.ts
@@ -1,5 +1,4 @@
 import DefaultTheme from 'vitepress/theme'
-import { hsButton } from '@huoshan-ui/vue'
 import { AntDesignContainer } from '@vitepress-demo-preview/component'
 import '@vitepress-demo-preview/component/dist/style.css'
 import './styles/vars.css'
@@ -7,6 +6,5 @@ import './styles/vars.css'
 export default {
   extends: DefaultTheme,
   enhanceApp({ app }) {
-    app.component('hsButton', hsButton)
   }
 }
